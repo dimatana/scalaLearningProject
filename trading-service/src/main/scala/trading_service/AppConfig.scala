fcd4e1dca4bd67bdccb294187388b61e66cc8b87
dimatana <dima.tanasciuc@gmail.com>
2026-07-30 11:42:25 +0200

    add tradingService

---
+++ trading-service/src/main/scala/trading_service/AppConfig.scala
@@ -0,0 +1,20 @@
+package trading_service
+
+import pureconfig.ConfigReader
+import pureconfig.generic.derivation.default.*
+import scala.concurrent.duration.FiniteDuration
+
+final case class AppConfig(
+    httpPort: Int,
+    httpShutdownTimeout: FiniteDuration,
+    dbUrl: String,
+    dbUser: String,
+    dbPassword: String,
+    dbMaxPoolSize: Int,
+    dbMinIdle: Int,
+    dbConnectionTimeout: FiniteDuration,
+) derives ConfigReader
+
+object AppConfig:
+  def load(): AppConfig =
+    pureconfig.ConfigSource.default.loadOrThrow[AppConfig]
---
+++ trading-service/src/main/scala/trading_service/EventApiDelegate.scala
@@ -0,0 +1,27 @@
+package trading_service
+
+import cats.effect.IO
+import trading_service.EventError
+import org.http4s.{Request, Response}
+import org.typelevel.log4cats.Logger
+import trading_service.generated.server.apis.DefaultApiDelegate
+import trading_service.generated.server.apis.DefaultApiDelegate.*
+import trading_service.generated.server.models.{Event as GenEvent, ErrorResponse as GenErrorResponse}
+
+final class EventApiDelegate(repo: EventRepository)(using logger: Logger[IO])
+  extends DefaultApiDelegate[IO]:
+
+  private def toGenEvent(e: Event): GenEvent =
+    GenEvent(eventId = e.eventId, name = e.name, betsPlaced = e.betsPlaced)
+
+  override def getEvent: getEvent = new getEvent:
+    def handle(req: Request[IO], id: Long, responses: getEventResponses[IO]): IO[Response[IO]] =
+      logger.info(s"GET /events/$id") *>
+        repo.findById(id).flatMap:
+          case Right(event)               => responses.resp200(toGenEvent(event))
+          case Left(_: EventError.NotFound) => responses.resp404(GenErrorResponse(s"Event $id not found"))
+          case Left(err)                  => responses.resp500(GenErrorResponse(err.message))
+
+  override def getHealth: getHealth = new getHealth:
+    def handle(req: Request[IO], responses: getHealthResponses[IO]): IO[Response[IO]] =
+      responses.resp200()
